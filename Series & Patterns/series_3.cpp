523a0d7071c3865d582905eec44bd742d95e1836
Anuj Attri <anujattri01@gmail.com>
2021-07-10 20:26:14 +0530

    Added series_3.cpp

---
+++ Series & Patterns/series_3.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+/*
+
+To find the sum of the following series:
+
+1 + (1/3^2) + (1/5^2) ... so on upto Nth term.
+
+*/
+
+int main() {
+
+    int N; float sum=0;
+    cout<<"Enter the number of terms: "; cin>>N;
+
+    for(int i=1; i<=N; i++) {
+
+        if (i % 2 != 0) {
+
+            sum += 1/pow(i, 2);
+        }
+    }
+
+    cout<<"The sum of the series for "<<N<<" terms is: "<<sum;
+}
