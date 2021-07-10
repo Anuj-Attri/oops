46779b387afb2a182234d6ac3e3f07d89c389545
Anuj Attri <anujattri01@gmail.com>
2021-07-10 18:59:50 +0530

    Updated "LCMandHCF.cpp"

--- Basic Programs/LCMandHCF.cpp
+++ Basic Programs/LCMandHCF.cpp
@@ -1,28 +1,25 @@
 #include <iostream>
 using namespace std;
 
-int LCM (int num1, int num2) {
-    int lcm;
-
-    for(int i=((num1*num2)-1); i>0; i--) {
-
-        if(i%num1 == 0 && i%num2 == 0){
-            lcm = i;
-        }
-        else {
-            lcm = num1*num2;
-        }
-    }
-    return lcm;
-}
-
 int main(){
 
-    int num1, num2, lcm;
+    int num1, num2, temp;
     cout<<"Enter the two numbers:"<<endl;
     cin>>num1>>num2;
+    temp = num1*num2;
 
-    lcm = LCM(num1, num2);
-    cout<<"The LCM is: "<<lcm;
+    do {
+        if (num1 > num2) {
+            num1 -= num2;
+        }
+
+        else {
+            num2 -= num1;
+        }
+    } while(num1 != num2);
+
+    cout<<"The LCM is: "<<num1<<endl;
+    cout<<"The HCF is: "<<temp/num1<<endl;
+
 }
 
