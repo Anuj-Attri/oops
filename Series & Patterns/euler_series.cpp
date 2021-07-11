614e8b6428d2535255ce96d790185cf339f5274c
Anuj Attri <anujattri01@gmail.com>
2021-07-11 19:27:57 +0530

    added verification in euler_series.cpp

--- Series & Patterns/euler_series.cpp
+++ Series & Patterns/euler_series.cpp
@@ -8,8 +8,18 @@ To find the euler series expansion for a given number of terms and verify the ou
 
 int main()  {
 
-    int x; float sum = 0;
-    cout<<"Enter the value of x: "; cin>>x;
+    int x, N, fact=1; float sum = 1;
 
-    for(int i=0; i<=)
+    cout<<"Enter the value of x: "; cin>>x;
+    cout<<"Enter the number of terms: "; cin>>N;
+
+    for(int i=1; i<=(N-1); i++) {
+        
+        fact *= i;
+        sum += (pow(x, i)/fact);
+    }
+
+    cout<<"The euler series expansion of x is: "<<sum<<endl;
+    cout<<"The exponent of x is: "<<exp(x)<<endl;
+    cout<<"Error: "<<(sum/exp(x)-1)*100<<"%"<<endl;
 }
