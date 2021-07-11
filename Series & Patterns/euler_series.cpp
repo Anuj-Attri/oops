e975e617ff0eeca588b8196d82f4ebb2c98c04d8
Anuj Attri <anujattri01@gmail.com>
2021-07-11 19:19:12 +0530

    Added "euler_series.cpp"

---
+++ Series & Patterns/euler_series.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+/*
+To find the euler series expansion for a given number of terms and verify the output.
+*/
+
+int main()  {
+
+    int x; float sum = 0;
+    cout<<"Enter the value of x: "; cin>>x;
+
+    for(int i=0; i<=)
+}
