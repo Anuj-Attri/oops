df4dd213c04be844324523380ad2da4162cf316c
Anuj Attri <anujattri01@gmail.com>
2021-07-11 18:28:26 +0530

    Added "days_to_months.cpp"

---
+++ Basic Programs/day_to_months.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+// Given the number of days, convert them to the corresponding number of years, months and days.
+
+int main()  {
+
+    int input_days, years, months, output_days;
+    cout<<"Enter the number of days: "; cin>>input_days;
+    
+
+}
