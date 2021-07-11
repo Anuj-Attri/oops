53861f05b0880a74f896f8a10e2fc4f8a826cf9f
Anuj Attri <anujattri01@gmail.com>
2021-07-11 20:37:04 +0530

    updated pattern_1.cpp

--- Series & Patterns/pattern_1.cpp
+++ Series & Patterns/pattern_1.cpp
@@ -14,5 +14,12 @@ using namespace std;
 int main()  {
 
     int N;
-    
+    cout<<"Enter the number of lines: "; cin>>N;
+
+    for(int i = 1; i<=N; i++)    {
+        for(int j = 1; j<=i; j++)   {
+            cout<<j<<"\t";
+        }
+        cout<<endl;
+    }
 }
