d278eb7d3a731564fbbee215d5c64c43e3da3ab9
Anuj Attri <anujattri01@gmail.com>
2021-07-16 21:55:04 +0530

    Updated palindrome.cpp

--- Strings/palindrome.cpp
+++ Strings/palindrome.cpp
@@ -3,16 +3,10 @@
 #include <string.h>
 using namespace std;
 
-int palindrome(char string[])   {
-
-    char string2 = strrev(string);
-    strcmp(string, string2)? cout<<"It is a palindrome.":cout<<"It is not a palindrome";
-
-    return 0;
-}
-
 int main()  {
-
-    cout<<"Enter the word: "
+    
+    char string[20] = "Hello World";
+    
+    strcmp(string, strrev(string))?cout<<"It is a palindrome.":cout<<"It is not a palindrome";
 
 }
