79400132b2139e8a013226eef3ec7a2993e047ea
Anuj Attri <anujattri01@gmail.com>
2021-07-23 22:54:04 +0530

    Updated palindrome.cpp

--- Strings/palindrome.cpp
+++ Strings/palindrome.cpp
@@ -5,10 +5,11 @@ using namespace std;
 
 int main()  {
     
-    char string[20];
+    char string[30], string_og[30];
     cout<<"Enter the word: ";
     gets(string);
-
-    (strcmp(string, strrev(string)) == 0) ? cout<<"It is a palindrome." : cout<<"It is not a palindrome";
+    strcpy(string_og, string);
+    // cout<<strrev(string)<<endl<<string;
+    (strcmp(string_og, strrev(string)) == 0) ? cout<<"It is a palindrome." : cout<<"It is not a palindrome";
 
 }
