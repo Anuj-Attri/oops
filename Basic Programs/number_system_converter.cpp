05350117d95c7ce458afdfe64ebc64c5d875e37f
Anuj Attri <anujattri01@gmail.com>
2021-07-29 18:43:53 +0530

    Added octal system to number_system_converter

--- Basic Programs/number_system_converter.cpp
+++ Basic Programs/number_system_converter.cpp
@@ -5,9 +5,9 @@ using namespace std;
 Create a progarm that can convert a given decimal number to binary and octal.
 */
 
-int decimal_to_binary(int n)    {
+int decimal_to_binary(int n, int iters)    {
 
-    int arr[8], i=0, len;
+    int arr[iters]={}, i=0, len;
     while(n!=0) {
         arr[i] = n%2;
         n /= 2;
@@ -15,13 +15,41 @@ int decimal_to_binary(int n)    {
     }
     len = (sizeof(arr)/sizeof(arr[0])) - 1;
     for(int j=len; j>=0; j-- ) {
-        cout<<arr[j]<<endl;
+        cout<<arr[j];
     }
     
 
 }
 
+int decimal_to_octal(int n, int iters)    {
+
+    int arr[iters]={}, i=0, len;
+    while(n!=0) {
+        arr[i] = n%8;
+        n /= 8;
+        i++;
+    }
+    len = (sizeof(arr)/sizeof(arr[0])) - 1;
+    for(int j=len; j>=0; j-- ) {
+        cout<<arr[j];
+    }
+    
+
+}
+
+
+int size_calc(int n)    {
+    int i=0;
+    while(n !=0)    {
+        n /= 2;
+        i++;
+    }
+
+    return i;
+}
+
 int main()  {
-    int n = 10;
-    decimal_to_binary(n);
+    int n = 39, iters;
+    iters = size_calc(n);
+    decimal_to_binary(n, iters);
 }
