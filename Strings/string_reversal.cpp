e8134e8715f0d4b302df81f6e74d0259f14e908d
Anuj Attri <anujattri01@gmail.com>
2021-07-29 17:36:54 +0530

    Updated string_reversal.cpp

--- Strings/string_reversal.cpp
+++ Strings/string_reversal.cpp
@@ -1,4 +1,13 @@
 #include<iostream>
-#include<cstdio>
 #include<string.h>
 using namespace std;
+
+// Reverse a string.
+
+int main()   {
+
+    char string[30];    
+    cout<<"Enter the string: "; gets(string);
+    strrev(string);
+    cout<<"Reversed String is: "<<string;
+}
