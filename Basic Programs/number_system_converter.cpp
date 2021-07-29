828f1c803ff82bb6a65e439e6481e15f94a6e098
Anuj Attri <anujattri01@gmail.com>
2021-07-29 19:05:53 +0530

    Debugged number_system_converter

--- Basic Programs/number_system_converter.cpp
+++ Basic Programs/number_system_converter.cpp
@@ -64,11 +64,11 @@ int main()  {
     here:
     cout<<"Choose between the following: \n1)Decimal to Binary \n2)Decimal to Octal \nYour choice: "; 
     cin>>choice;
-    system(CLS);
+    system("CLS");
     cout<<"Enter the number: "; cin>>n;
     iters = size_calc(n, choice);
-    system(CLS);
-    
+    system("CLS");
+
     switch(choice)  {
         case 1:
             decimal_to_binary(n, iters);
